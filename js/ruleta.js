e52eed405ae9da638802f901b4ed7357337d04f2
rodrifer27 <88859986+rodrifer27@users.noreply.github.com>
2023-04-23 20:54:15 -0400

    Update ruleta.js

--- js/ruleta.js
+++ js/ruleta.js
@@ -1,33 +1,25 @@
-const prevButton = $('#prev-button');
-const nextButton = $('#next-button');
-const images = $('#image-container img');
-let currentIndex = 0;
-
-function showImage(index) {
-  images.eq(currentIndex).removeClass('active');
-  images.eq(index).addClass('active');
-  currentIndex = index;
-  adjustButtonPosition();
-}
-
-function adjustButtonPosition() {
-  const activeImage = $('#image-container img.active');
-  const buttonContainer = $('.button-container');
-  buttonContainer.css('top', `${activeImage.offset().top + activeImage.height() / 2}px`);
-}
-
-prevButton.on('click', () => {
-  let index = currentIndex - 1;
-  if (index < 0) {
-    index = images.length - 1;
-  }
-  showImage(index);
-});
-
-nextButton.on('click', () => {
-  let index = currentIndex + 1;
-  if (index >= images.length) {
-    index = 0;
-  }
-  showImage(index);
+$(document).ready(function() {
+  $('.carousel').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    arrows: false,
+    appendDots: $('.botones'),
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
